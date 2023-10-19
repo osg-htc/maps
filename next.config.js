5edd1d139360b81aabbf95fe5d6fdd12e0098f87
Rhymlore <bakirci.alperen@gmail.com>
2023-10-19 10:38:06 -0500

    added workflow to deploy github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    images: {
+        loader: "custom",
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    },
+    transpilePackages: ["next-image-export-optimizer"],
+    env: {
+        nextImageExportOptimizer_imageFolderPath: "/public/",
+        nextImageExportOptimizer_exportFolderPath: "out",
+        nextImageExportOptimizer_quality: 100,
+        nextImageExportOptimizer_storePicturesInWEBP: true,
+        nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+
+        // If you do not want to use blurry placeholder images, then you can set
+        // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+        // `placeholder="empty"` to all <ExportedImage> components.
+        nextImageExportOptimizer_generateAndUseBlurImages: true,
+    },
+}
 
 module.exports = nextConfig
