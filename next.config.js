2951727426e997a383074275cabcc848ff18876d
Rhymlore <bakirci.alperen@gmail.com>
2023-10-19 11:24:58 -0500

    export out

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+  }
+   
+  module.exports = nextConfig
