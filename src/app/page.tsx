96f7c0420a00015f804e8e986a45173bbe894d72
Cannon Lock <clock@wisc.edu>
2023-10-26 22:12:11 -0500

    Add a a globe map with some points from the geojson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 "use client"
-import { GlobeComponent } from '@/components/mapbox';
-import Head from "next/head";
-import { Box, Autocomplete, TextField, Container  } from '@mui/material';
 
+
+import Head from "next/head";
+import { useMemo } from "react";
+import { Box, Autocomplete, TextField, Container  } from '@mui/material';
+import { Map, Marker } from "react-map-gl"
+import Features from "../../public/features.json"
+
+import {BusAlert} from "@mui/icons-material";
+
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function HomePage() {
+
+  let markers = useMemo(() => {
+    return Features.features.map((feature) => {
+      return (
+        <Marker
+          key={feature.id}
+          longitude={feature.geometry.coordinates[0]}
+          latitude={feature.geometry.coordinates[1]}
+        >
+          <BusAlert/>
+        </Marker>
+      );
+    });
+  }, [Features])
+
   return (
     <>
       <Head>
@@ -14,11 +36,15 @@ function HomePage() {
         />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-    <Container>
-    <Box>
-        <GlobeComponent/>
-    </Box>  
-    </Container>
+      <Box height={"100vh"} width={"100vw"}>
+        <Map
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+          projection={"globe"}
+        >
+          {markers}
+        </Map>
+      </Box>
     </>
   );
 }
