5894716b9881d182e9c2e04b2f8e1c81dd3b1931
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2024-11-15 15:26:28 -0600

    Adjust styling

--- src/app/components/DataCard.tsx
+++ src/app/components/DataCard.tsx
@@ -6,32 +6,35 @@ type DataCardProps = {
   numberOfProjects?: number;
 }
 
-const DataCard: React.FC<DataCardProps> = ( {numberOfInstitutions, shifted, numberOfProjects}) => {
-
+const DataCard: React.FC<DataCardProps> = ({ numberOfInstitutions, shifted, numberOfProjects }) => {
 
 
   return (
-    <Card sx={{
-      position: 'absolute',
-      bottom: '35px',
-      left: shifted ? '90%' : '0.5%',
-      transition: 'left 0.3s ease-in-out',
-      zIndex: 2,
-    }}>
+    <Card
+      variant="outlined"
+      sx={{
+        display: 'flex',
+        position: 'absolute',
+        bottom: '35px',
+        left: shifted ? '90%' : '0.5%',
+        transition: 'left 0.3s ease-in-out',
+        zIndex: 2,
+        placeItems: 'center',
+        justifyContent: 'center',
+        height: '60px',
+      }}>
       <CardContent>
-        <Typography gutterBottom sx={{ color: 'darkorange', fontSize: 14 }}>
+        <Typography gutterBottom sx={{ color: 'darkorange', fontSize: 14}}>
           Institutions: {numberOfInstitutions}
         </Typography>
 
-        {numberOfProjects && <Typography gutterBottom sx={{ color: 'darkorange', fontSize: 14 }}>
+        {numberOfProjects && <Typography gutterBottom sx={{ color: 'darkorange', fontSize: 14}}>
           Projects: {numberOfProjects}
         </Typography>}
       </CardContent>
     </Card>
-  )
-}
-
-
+  );
+};
 
 
 export default DataCard;
