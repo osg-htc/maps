c83b390edc6cdfcfa4b0246cc218b40634e47a82
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2024-11-19 17:44:56 -0600

    Single out the server-side data fetching and move the code for transforming the data that requires using react hooks to a client component. Also use useSWR hook to get the server-fetched data

---
+++ src/app/projects/config.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import React, { useMemo } from 'react';
+import useSWR from 'swr';
+import MapComponent from '@/app/projects/MapComponent';
+// @ts-ignore
+import { Institution, Project, ProjectWithESData, InstitutionWithProjects } from '@/types/mapTypes';
+import esProjects from '@/data/esProjects';
+
+
+const fetchElasticsearchProjects = async () => {
+  const response = await esProjects();
+  return response.aggregations.projects.buckets;
+};
+
+const fetcher = (key: string) => {
+  if (key === 'projects') {
+    return fetch('https://topology.opensciencegrid.org/miscproject/json').then((res) => res.json());
+  } else if (key === 'institutions') {
+    return fetch('https://topology-institutions.osg-htc.org/api/institution_ids').then((res) => res.json());
+  } else if (key === 'esProjects') {
+    return fetchElasticsearchProjects();
+  }
+}
+
+const Config: React.FC<{
+  initialInstitutions: Institution[];
+  initialProjects: Project[];
+  initialEsProjects: any[];
+}> = ({ initialInstitutions, initialProjects, initialEsProjects }) => {
+
+  // console.log('initialInstitutions', initialInstitutions);
+  // console.log('initialProjects', initialProjects);
+  // console.log('initialEsProjects', initialEsProjects);
+  const { data: institutions } = useSWR('institutions', fetcher, {
+    fallbackData: initialInstitutions,
+  });
+
+  let { data: projects } = useSWR('projects', fetcher, {
+    fallbackData: initialProjects,
+  });
+
+  const { data: esProjects } = useSWR('esProjects', fetcher, {
+    fallbackData: initialEsProjects,
+  });
+
+  projects = useMemo(() => {
+    if (!projects) return [];
+    if (Array.isArray(projects)) {
+      return projects;
+    } else if (typeof projects === 'object' && projects !== null) {
+      return Object.values(projects);
+    } else {
+      console.error('Unexpected projectsData format:', projects);
+      return [];
+    }
+  }, [projects]);
+
+  // console.log('institutions', institutions);
+  // console.log('projects', projects);
+
+  const filteredProjects = useMemo(() => {
+    const projectNames = new Set(
+      (esProjects || []).map((project: Project) => project.key)
+    );
+    return (projects || []).filter((project: Project) => projectNames.has(project.Name));
+  }, [esProjects, projects]);
+
+  const institutionsWithProjects: InstitutionWithProjects[] = useMemo(() => {
+    return (institutions || []).map((institution: Institution) => {
+      const institutionProjects: ProjectWithESData[] = filteredProjects
+        .filter((project: Project) => project.InstitutionID === institution.id)
+        .map((proj: Project) => {
+          const projectData = (esProjects || []).find((elProj: Project) => elProj.key === proj.Name);
+          return {
+            ...proj,
+            esData: {
+              docCount: projectData?.doc_count || 0,
+              cpuHours: projectData?.projectCpuUse.value || 0,
+              gpuHours: projectData?.projectGpuUse.value || 0,
+              jobsRan: projectData?.projectJobsRan.value || 0,
+            }
+          };
+        });
+
+      return {
+        ...institution,
+        projects: institutionProjects
+      };
+    }).filter((iwp: { projects: string | any[]; }) => iwp.projects.length > 0);
+  }, [institutions, filteredProjects, esProjects]);
+
+  // console.log('institutionsWithProjects', institutionsWithProjects);
+
+  return (
+    <MapComponent
+      institutions={institutions}
+      projects={projects}
+      esProjects={esProjects}
+      institutionsWithProjects={institutionsWithProjects}
+      filteredProjects={filteredProjects}
+    />
+  );
+}
+
+export default Config;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,7 +10,7 @@ import useSWR from 'swr';
 // @ts-ignore
 import { Institution, Project, ProjectWithESData, InstitutionWithProjects } from '@/types/mapTypes';
 import esProjects from '@/data/esProjects';
-import MapComponent from '@/app/projects/MapComponent';
+import Config from './config';
 
 // const DynamicProjects = dynamic(
 //   () => import('./MapComponent'),
@@ -31,82 +31,18 @@ const fetchElasticsearchProjects = async () => {
   return response.aggregations.projects.buckets;
 };
 
-  const fetcher = (key: string) => {
-    if (key === 'projects') {
-      return fetch('https://topology.opensciencegrid.org/miscproject/json').then((res) => res.json());
-    } else if (key === 'institutions') {
-      return fetch('https://topology-institutions.osg-htc.org/api/institution_ids').then((res) => res.json());
-    } else if (key === 'esProjects') {
-      return fetchElasticsearchProjects();
-    } else{
-      throw new Error(`Unknown key: ${key}`);
-    }
-  }
+const Page= async() => {
+  const institutionsResponse = await fetch('https://topology-institutions.osg-htc.org/api/institution_ids');
+  const institutions: Institution[] = await institutionsResponse.json();
 
+  const projectsResponse = await fetch('https://topology.opensciencegrid.org/miscproject/json');
+  let projects: Project[] = await projectsResponse.json();
 
-const Page: React.FC<{institutions: Institution[]; projects: Project[]; esProjects: any[]}> = ({ institutions: initialInstitutions, projects: initialProjects, esProjects: initialEsProjects}) => {
-  console.log('initialInstitutions', initialInstitutions);
+  const esProjects = await fetchElasticsearchProjects();
 
-  const { data: institutions } = useSWR('institutions', fetcher, {
-    fallbackData: initialInstitutions,
-  });
-
-  let { data: projects } = useSWR('projects', fetcher, {
-    fallbackData: initialProjects,
-  });
-
-  let { data: esProjects } = useSWR('esProjects', fetcher, {
-    fallbackData: initialEsProjects,
-  });
-
-  // console.log('projects', projects);
-  // console.log('elasticsearchProjects', esProjects);
-  // console.log('institutions', institutions);
-
-  projects = useMemo(() => {
-    if (!projects) return [];
-    if (Array.isArray(projects)) {
-      return projects;
-    } else if (typeof projects === 'object' && projects !== null) {
-      return Object.values(projects);
-    } else {
-      console.error('Unexpected projectsData format:', projects);
-      return [];
-    }
-  }, [projects]);
-
-  const filteredProjects = useMemo(() => {
-    const projectNames = new Set(
-      (esProjects || []).map((project: Project) => project.key)
-    );
-    return (projects || []).filter((project: Project) => projectNames.has(project.Name));
-  }, [esProjects, projects]);
-
-  const institutionsWithProjects: InstitutionWithProjects[] = useMemo(() => {
-    return (institutions || []).map((institution: Institution) => {
-      const institutionProjects: ProjectWithESData[] = filteredProjects
-        .filter((project: Project) => project.InstitutionID === institution.id)
-        .map((proj: Project) => {
-          const projectData = (esProjects || []).find((elProj: Project) => elProj.key === proj.Name);
-          return {
-            ...proj,
-            esData: {
-              docCount: projectData?.doc_count || 0,
-              cpuHours: projectData?.projectCpuUse.value || 0,
-              gpuHours: projectData?.projectGpuUse.value || 0,
-              jobsRan: projectData?.projectJobsRan.value || 0,
-            }
-          };
-        });
-
-      return {
-        ...institution,
-        projects: institutionProjects
-      };
-    }).filter((iwp: { projects: string | any[]; }) => iwp.projects.length > 0);
-  }, [institutions, filteredProjects, esProjects]);
-
-  // console.log('institutionsWithProjects', institutionsWithProjects);
+  // console.log(institutions)
+  //
+  // projects = Array.isArray(projects) ? projects : Object.values(projects);
 
   return (
     <>
@@ -119,7 +55,9 @@ const Page: React.FC<{institutions: Institution[]; projects: Project[]; esProjec
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <Box height={'100vh'} width={'100vw'}>
-        <MapComponent institutions={institutions} projects={projects} esProjects={esProjects} institutionsWithProjects={institutionsWithProjects} filteredProjects={filteredProjects}/>
+        <Config initialInstitutions={institutions}
+                initialProjects={projects}
+                initialEsProjects={esProjects}/>
       </Box>
     </>
   );
