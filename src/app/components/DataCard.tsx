df7128f58a99ea9ac69c9d5ffc380df48a87e267
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2024-11-22 14:53:04 -0600

    Add switch to switch between institutions and projects page

--- src/app/components/DataCard.tsx
+++ src/app/components/DataCard.tsx
@@ -1,5 +1,15 @@
-import { Box, Button, Card, CardActions, CardContent, Typography, useMediaQuery } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  FormControlLabel, Switch,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
 import useTheme from '@mui/material/styles/useTheme';
+import { useState } from 'react';
 
 type DataCardProps = {
   numberOfInstitutions: number;
@@ -9,66 +19,65 @@ type DataCardProps = {
 
 const DataCard: React.FC<DataCardProps> = ({ numberOfInstitutions, shifted, numberOfProjects }) => {
   const theme = useTheme();
+  const [page, setPage] = useState<string>(() => {
+    if (typeof window !== 'undefined') {
+      return window.location.pathname.includes('projects') ? 'projects' : 'institutions';
+    }
+    return 'institutions';
+  }); // 'institutions' or 'projects'
+  const handlePageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newPage = event.target.checked ? 'institutions' : 'projects';
+    setPage(newPage);
+    window.history.pushState({}, '', `/maps/${newPage}`);
+    window.location.reload();
+  };
 
   return (
     <Box>
-    <Card
-      variant="outlined"
-      sx={{
-        display: 'flex',
-        position: 'absolute',
-        bottom: theme.breakpoints.down('sm') ? '30px' : '35px',
-        left: shifted
-          ? {
-            xs: '50%',
-            sm: '78%',
-            md: '85%',
-            lg: '88%',
-            xl: '91%',
-          }
-          : {
-            xs: '2%',
-            sm: '0.5%',
-            md: '0.5%',
-            lg: '0.5%',
-            xl: '0.5%',
-          },
-        width: {
-          xs: '65%',
-          sm: '38%',
-          md: '28%',
-          lg: '24%',
-          xl: '15%',
-        },
-        transform: theme.breakpoints.down('sm') && shifted ? 'translateX(-50%)' : 'none',
-        transition: 'left 0.6s ease-in-out',
-        zIndex: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '60px',
-        borderRadius: '8px',
-      }}>
-      <CardContent sx={{
-        display: 'flex',
-        flexDirection: {
-          xs: 'row',
-          sm: 'column',
-        },
-        alignItems: 'center',
-      }}>
-        <Typography
-          gutterBottom
-          sx={{
-            color: 'darkorange',
-            fontSize: 14,
-            '&:hover': {
-              color: 'black',
+      <Card
+        variant="outlined"
+        sx={{
+          display: 'flex',
+          position: 'absolute',
+          bottom: theme.breakpoints.down('sm') ? '30px' : '35px',
+          left: shifted
+            ? {
+              xs: '50%',
+              sm: '78%',
+              md: '85%',
+              lg: '88%',
+              xl: '91%',
+            }
+            : {
+              xs: '2%',
+              sm: '0.5%',
+              md: '0.5%',
+              lg: '0.5%',
+              xl: '0.5%',
             },
-          marginRight: 1}}>
-          Institutions: {numberOfInstitutions}
-        </Typography>
-
-        {numberOfProjects &&
+          width: {
+            xs: '70%',
+            sm: '38%',
+            md: '28%',
+            lg: '24%',
+            xl: '15%',
+          },
+          transform: theme.breakpoints.down('sm') && shifted ? 'translateX(-50%)' : 'none',
+          transition: 'left 0.6s ease-in-out',
+          zIndex: 2,
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '60px',
+          borderRadius: '8px',
+        }}>
+        <CardContent sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'row',
+            sm: 'column',
+          },
+          alignItems: 'center',
+        }}>
           <Typography
             gutterBottom
             sx={{
@@ -77,29 +86,78 @@ const DataCard: React.FC<DataCardProps> = ({ numberOfInstitutions, shifted, numb
               '&:hover': {
                 color: 'black',
               },
-            }}>
-          Projects: {numberOfProjects}
-        </Typography>}
-      </CardContent>
+              marginRight: 1,
+            justifyContent: 'center'}}>
+            Institutions: {numberOfInstitutions}
+          </Typography>
 
-      <CardActions>
-        <Button variant="contained"
-                href="https://osg-htc.org/"
+          {numberOfProjects &&
+            <Typography
+              gutterBottom
+              sx={{
+                color: 'darkorange',
+                fontSize: 14,
+                '&:hover': {
+                  color: 'black',
+                },
+              }}>
+              Projects: {numberOfProjects}
+            </Typography>}
+        </CardContent>
+
+        <CardActions sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 1, // Adds spacing between the button and switch
+        }}>
+          <Button variant="contained"
+                  href="https://osg-htc.org/"
+                  sx={{
+                    fontSize: 10,
+                    padding: '10px 16px',
+                    backgroundColor: 'darkorange',
+                    '&:hover': {
+                      backgroundColor: 'black',
+                    },
+                    width: '60%',
+                    height: '15px'
+                  }}>
+            OSPOOL
+          </Button>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={page === 'institutions'}
+                onChange={handlePageChange}
+                name="pageSwitch"
+                size='small'
+                color='warning'
                 sx={{
-                  fontSize: 10,
-                  padding: '10px 8px',
-                  backgroundColor: 'darkorange',
-                  '&:hover': {
-                    backgroundColor: 'black',
-                  },
-                }}>
-          OSPOOL
-        </Button>
-      </CardActions>
-    </Card>
+                  color: 'darkorange',
+                  fontSize: 5,
+                }}
+              />
+            }
+            label={page === 'institutions' ? "Institutions" : "Projects"}
+            sx={{
+              '& .MuiFormControlLabel-label': {
+                fontSize: '14px',
+              },
+              '&:hover': {
+                color: 'black',
+              },
+              width: '100%',
+              color: 'darkorange',
+              margin: 0,
+              height: '15px',
+              justifyContent: 'center',
+            }}
+          />
+        </CardActions>
+      </Card>
     </Box>
   );
 };
-
 
 export default DataCard;
