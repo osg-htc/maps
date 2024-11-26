3445aa50908070fedcf5c32926bdefe4664fbb88
Cannon Lock <clock@wisc.edu>
2024-11-26 08:51:17 -0600

    Filter out Projects

--- src/data/esProjects.tsx
+++ src/data/esProjects.tsx
@@ -3,7 +3,7 @@ import ElasticSearchQuery, { DATE_RANGE, OSPOOL_FILTER } from './eqProjects';
 const esProjects = async () => {
   const elasticSearch = new ElasticSearchQuery();
 
-  let usageQueryResult = elasticSearch.search({
+  let usageQueryResult = await elasticSearch.search({
     size: 0,
     query: {
       bool: {
@@ -49,6 +49,19 @@ const esProjects = async () => {
       },
     },
   });
+
+  const ospool_projects: Set<string> = new Set(await(await fetch("https://osg-htc.org/ospool-data/data/ospool_projects.json")).json())
+  const osgconnect_projects: Set<string> = new Set(await(await fetch("https://osg-htc.org/assets/data/osgconnect_projects.json")).json())
+  const validProjects = new Set([...ospool_projects, ...osgconnect_projects])
+
+  // Remove invalid projects from es return
+  for (let i = 0; i < usageQueryResult.aggregations.projects.buckets.length; i++) {
+    if (!validProjects.has(usageQueryResult.aggregations.projects.buckets[i].key)) {
+      usageQueryResult.aggregations.projects.buckets.splice(i, 1);
+      i--;
+    }
+  }
+
   return usageQueryResult;
 };
 
