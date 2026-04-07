a8b28398495663e31ee1d9c9a84161d33c836e9e
mattriggle05 <mattriggle05@gmail.com>
2026-04-07 15:30:59 -0500

    Remove map control disabler, no needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import CenterOverlay from "@/src/components/CenterOverlay";
-import DisableMapControls from "@/src/components/DisableMapControls";
 import { Link } from "@mui/material";
 
 export default async function Home() {
 
   return (
     <>
-      <DisableMapControls />
       <CenterOverlay>
         <Link href="./projects">Projects</Link>
       </CenterOverlay>
