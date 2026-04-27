63ca10c31804df7c99ede7cbaeea595bdbec22fd
mattriggle05 <mattriggle05@gmail.com>
2026-04-27 13:31:13 -0500

    Add suspense around search params use in the map. Bad idea? Maybe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-
 import { Box } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-
 import theme, {fonts} from "@chtc/web-components/themes/osg"
-
 import "@/src/app/globals.css"
 import Analytics from "@/src/components/Analytics";
 import BaseMap from "@/src/components/BaseMap";
+import { Suspense } from "react";
+import LoadingScreen from "../components/LoadingScreen";
 
 export const metadata: Metadata = {
   title: "Website Template",
@@ -30,9 +29,11 @@ export default function RootLayout({
         <Box component={"body"} sx={{ margin: 0, padding: 0 }}>
           <ThemeProvider theme={theme}> 
             <Box sx={{ position: 'relative', width: '100vw', height: '100vh', bgcolor: "background.default" }}>
-              <BaseMap>
-                {children}
-              </BaseMap>
+              <Suspense fallback={<LoadingScreen />}>
+                <BaseMap>
+                  {children}
+                </BaseMap>
+              </Suspense>
             </Box>
           </ThemeProvider>
         </Box>
