f4b37bf26786b2eec1221dbb6c697d0437f5a23e
mattriggle05 <mattriggle05@gmail.com>
2026-05-18 10:29:29 -0500

    Set up institution map page & folder

---
+++ src/components/InstitutionsMap/Example.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+export default function Example() {
+  return (
+    <Typography>Hello, World!</Typography>
+  )
+}
---
+++ src/components/InstitutionsMap/index.tsx
@@ -0,0 +1,10 @@
+import Sidebar from '../Sidebar';
+import Example from './Example';
+
+export default function Map() {
+  return (
+    <Sidebar>
+      <Example></Example>
+    </Sidebar>
+  );
+}
